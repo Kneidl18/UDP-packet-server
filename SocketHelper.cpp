2193881653e0efb286b0919e8afee93d89640010
Andreas Auer <38904011+Kneidl18@users.noreply.github.com>
2024-03-20 09:06:02 +0100

    work out basic structure

--- SocketHelper.cpp
+++ SocketHelper.cpp
@@ -6,7 +6,9 @@
 #include <iostream>
 
 void fillPacket(uint16_t tId, uint32_t seqNum, uint8_t *data, size_t dataLen);
+
 // TODO: fileNameLen 1-256 byte!!
+// TODO: 65527 Byte of data per package
 bool sendMsg(uint8_t *data, size_t dataLen, uint8_t *fileName, size_t fileNameLen);
 
 // TODO: i would suggest to make the checksum of the filename + data (all concatenated together)
