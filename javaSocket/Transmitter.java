0c69ec83281bd55162bc15bdf9605b9161005fd4
Alexander Neubacher <alexander.neubacher@stud.plus.ac.at>
2024-03-22 09:26:38 +0100

    Grundstruktur hergestellt

--- javaSocket/Transmitter.java
+++ javaSocket/Transmitter.java
@@ -1,4 +1,22 @@
 package javaSocket;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Transmitter {
+
+    public static void main(String[] args) throws IOException {
+        send("heute ist ein schoener tag", "127.0.0.1", 3000);
+    }
+
+    public static void send(String message, String ipAddress, int port) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress address = InetAddress.getByName(ipAddress);
+
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), address, port);
+        socket.send(packet);
+        socket.close();
+    }
+
+
 }
