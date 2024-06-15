0524a92b219e4388993d30e216a9d6a892d7808d
Andreas Auer <38904011+Kneidl18@users.noreply.github.com>
2024-06-15 21:40:17 +0200

    implement stop and wait protocol

--- cpp/SocketHelper.h
+++ cpp/SocketHelper.h
@@ -5,72 +5,78 @@
 #ifndef PP_SOCKETHELPER_H
 #define PP_SOCKETHELPER_H
 
-#include "configuration.h"
-#include <iostream>
-#include <queue>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <list>
 #include "PacketStructure.h"
-#include <variant>
+#include "configuration.h"
 #include <cstring>
+#include <iostream>
+#include <list>
+#include <netinet/in.h>
+#include <queue>
+#include <sys/socket.h>
+#include <variant>
 
 class SocketHelper {
 private:
-    std::queue<PacketVariant> packetQueue;
-    uint16_t transmissionId = 0;
-    bool msgSend = true;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in *dstIpAddr = nullptr;
-    std::vector<Transmission *> incomingTransmission;
-    std::string outputDir;
-    bool verboseOutput;
+  std::queue<PacketVariant> packetQueue;
+  uint16_t transmissionId = 0;
+  bool msgSend = true;
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in *dstIpAddr = nullptr;
+  std::vector<Transmission *> incomingTransmission;
+  std::string outputDir;
+  bool verboseOutput;
 
-    static void fillPacketHeader(PacketHeader *packetHeader, uint16_t tId, uint32_t seqNum);
-    static void fillPacket(Packet *packet, PacketHeader *packetHeader, uint8_t *data, size_t dataLen);
-    static void fillStartPacket(StartPacket *packet, PacketHeader *packetHeader, size_t n, uint8_t *fileName, size_t nameLen);
-    static void fillEndPacket(EndPacket *packet, PacketHeader *packetHeader, const uint8_t *checksum);
-    static void increaseSequenceNumber(PacketHeader *header);
+  static void fillPacketHeader(PacketHeader *packetHeader, uint16_t tId,
+                               uint32_t seqNum);
+  static void fillPacket(Packet *packet, PacketHeader *packetHeader,
+                         uint8_t *data, size_t dataLen);
+  static void fillStartPacket(StartPacket *packet, PacketHeader *packetHeader,
+                              size_t n, uint8_t *fileName, size_t nameLen);
+  static void fillEndPacket(EndPacket *packet, PacketHeader *packetHeader,
+                            const uint8_t *checksum);
+  static void increaseSequenceNumber(PacketHeader *header);
 
-    static void calcChecksum(EndPacket *endPacket, uint8_t *data, size_t dataLen, uint8_t *fileName,
-                      size_t fileNameLen);
-    static void calcChecksumFromTransmission(Transmission *transmission, EndPacket *endPacket);
+  static void calcChecksum(EndPacket *endPacket, uint8_t *data, size_t dataLen,
+                           uint8_t *fileName, size_t fileNameLen);
+  static void calcChecksumFromTransmission(Transmission *transmission,
+                                           EndPacket *endPacket);
 
-    static bool checkCorrectnessOfPackets(StartPacket *startPacket, Packet *packets, EndPacket *endPacket);
-    static bool checkCorrectnessOfTransmission(Transmission *t);
+  static bool checkCorrectnessOfPackets(StartPacket *startPacket,
+                                        Packet *packets, EndPacket *endPacket);
+  static bool checkCorrectnessOfTransmission(Transmission *t);
 
-    bool pushToPacketQueue(PacketVariant packet);
-    bool pushToIncomingQueue(char *buffer, ssize_t len);
+  bool pushToPacketQueue(PacketVariant packet);
+  bool pushToIncomingQueue(char *buffer, ssize_t len);
 
-    void createSocketRecv(int *socket1);
-    void createSocketSend(int *socket1);
+  void createSocket(int *sock_fd, bool send);
 
-    void runMaster();
-    void runSlave(const bool *run);
+  void sendPacket(int sock_fd, char *buffer, ssize_t len);
+  void runMaster();
+  void runSlave(const bool *run);
 
-    void processIncomingMsg(Transmission *t);
-    void checkFinishedTransmission();
+  void processIncomingMsg(Transmission *t);
+  void checkFinishedTransmission();
 
-    static void sortPackets(Packet *packets, size_t n);
-    static void sortPackets(Transmission *t);
+  static void sortPackets(Packet *packets, size_t n);
+  static void sortPackets(Transmission *t);
 
-    bool savePacketsToFile(StartPacket *startPacket, Packet *packets);
-    bool saveTransmissionToFile(Transmission *t);
+  bool savePacketsToFile(StartPacket *startPacket, Packet *packets);
+  bool saveTransmissionToFile(Transmission *t);
 
 public:
-    void run(const bool *run, Config config);
+  void run(const bool *run, Config config);
 
-    bool sendMsg(uint8_t *data, size_t dataLen, uint8_t *fileName, size_t fileNameLen);
+  bool sendMsg(uint8_t *data, size_t dataLen, uint8_t *fileName,
+               size_t fileNameLen);
 
-    [[nodiscard]] bool msgOut() const;
+  [[nodiscard]] bool msgOut() const;
 
-    void setIpSettings(uint8_t *dstIp, size_t port);
+  void setIpSettings(uint8_t *dstIp, size_t port);
 
-    void setOutputDirPath(std::string outDir);
+  void setOutputDirPath(std::string outDir);
 
-    void enableVerboseOutput() { verboseOutput = true; }
-    void disableVerboseOutput() { verboseOutput = false; }
+  void enableVerboseOutput() { verboseOutput = true; }
+  void disableVerboseOutput() { verboseOutput = false; }
 };
 
-
-#endif //PP_SOCKETHELPER_H
+#endif // PP_SOCKETHELPER_H
--- cpp/configuration.h
+++ cpp/configuration.h
@@ -5,11 +5,19 @@
 #ifndef PP_CONFIGURATION_H
 #define PP_CONFIGURATION_H
 
-typedef enum {
-    SLAVE,
-    MASTER
-}Config;
+typedef enum { SLAVE, MASTER } Config;
 
-#define CONFIGURATION SLAVE
+// timeout waiting for an ACK
+#define TIMEOUT 1000
 
-#endif //PP_CONFIGURATION_H
+// default port number
+#define PORT_NUMBER 8080
+
+// maximum data per packet
+#define MAX_DATA_LEN 1000.0
+#define BUFFER_LEN (uint) MAX_DATA_LEN
+
+// millisecond a packet is kept before it's deleted from the vector
+#define PACKET_TIMEOUT 1000
+
+#endif // PP_CONFIGURATION_H
